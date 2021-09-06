afdf071bb5562b817f7066006f3cfe867028fb6c
Mikko Pääkkönen <mikko@live.savonia.fi>
2021-09-06 17:02:12 +0300

    initial working version

--- Program.cs
+++ Program.cs
@@ -1,17 +1,30 @@
-﻿using System;
+﻿#nullable enable
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Threading.Tasks;
 using CommandLine;
+using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace net_console_app_base
 {
     class Options
     {
-        [Option('i', "input", Required = false, HelpText = "Help for input")]
-        public string Input { get; set; }
+        [Option('r', "repos", Required = false, HelpText = "Repos folder (uses current folder if none is specified")]
+        public string ReposFolder { get; set; }
 
-        [Option('o', "output", Required = true, HelpText = "Help for output")]
-        public string OutputFile { get; set; }
+        [Option('c', "comments-csv", Required = true, HelpText = "A csv file with columns for gh repo folder and the comment")]
+        public string CommentsCsv { get; set; }
+
+        [Option('i', "id-column", Required = false, HelpText = "The GitHub account id column name (default: id)")]
+        public string IdColumn { get; set; } = "id";
+
+        [Option('f', "feedback-column", Required = false, HelpText = "The feedback / comment column name (default: comment)")]
+        public string FeedbackColumn { get; set; } = "comment";
+
     }
 
     class Program
@@ -20,7 +33,7 @@ namespace net_console_app_base
         {
             var result = await CommandLine.Parser.Default.ParseArguments<Options>(args)
               .WithParsedAsync(RunOptions);
-            result.WithNotParsed(HandleParseError);        
+            result.WithNotParsed(HandleParseError);
         }
 
         /// <summary>
@@ -30,13 +43,75 @@ namespace net_console_app_base
         /// <returns></returns>
         static async Task RunOptions(Options opts)
         {
-            // check if valid arguments were prodived...
-            if (string.IsNullOrEmpty(opts.Input))
+            CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture)
             {
-                // do something... e.g. set default 
+                Delimiter = ";",
+                HasHeaderRecord = true,
+            };
+
+            if (string.IsNullOrEmpty(opts.ReposFolder))
+            {
+                opts.ReposFolder = System.IO.Directory.GetCurrentDirectory();
             }
 
-            // rest of logic...
+            if (false == System.IO.File.Exists(opts.CommentsCsv))
+            {
+                Console.WriteLine($"Comments csv file ({opts.CommentsCsv}) does not exist.");
+                return;
+            }
+
+            // read comments file
+            using (var reader = new StreamReader(opts.CommentsCsv))
+            {
+                int counter = 1;
+                await foreach (var line in ReadAsync(reader, config))
+                {
+                    IDictionary<string, Object> lineData = (IDictionary<string, Object>)line;
+                    // process a line from comments csv
+                    Console.WriteLine($"{counter++}: {lineData[opts.IdColumn]}: {lineData[opts.FeedbackColumn]}");
+
+                    string path = Path.Combine(opts.ReposFolder, lineData[opts.IdColumn]?.ToString());
+                    if (Directory.Exists(path))
+                    {
+                        var p = ExecuteCommand($"pr comment --body \"{lineData[opts.FeedbackColumn]}\"", path);
+                        if (p is not null)
+                        {
+                            await p.WaitForExitAsync();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Directory for answer repo ({path}) does not exist.");
+                    }
+                }
+            }
+        }
+
+        private static async IAsyncEnumerable<dynamic> ReadAsync(StreamReader reader, CsvConfiguration config)
+        {
+            using (var csv = new CsvReader(reader, config))
+            {
+                var records = csv.GetRecordsAsync<dynamic>();
+                await foreach (var line in records)
+                {
+                    yield return line;
+                }
+            }
+        }
+
+        private static Process? ExecuteCommand(string command, string path)
+        {
+            // Console.WriteLine($"would execute: {command} in {path}");
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("gh.exe", command);
+            processInfo.WorkingDirectory = path;
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = true;
+
+            process = Process.Start(processInfo);
+            return process;
         }
 
         static void HandleParseError(IEnumerable<Error> errs)
--- README.md
+++ README.md
@@ -1,7 +1,30 @@
-# net-console-app-base
+# GitHub Classroom assingment PR comment helper
 
-A template for .NET console app with command line arguments
+This tool is used to provide comments in bulk to GHC assingment answers from a csv file.
+The tool matches the answer repo with the GH username in the csv file and creates a PR comment from the comment text in the csv file.
 
-Uses https://www.nuget.org/packages/CommandLineParser/ for command line argument parsing.
+## Requirements
 
-Help via https://github.com/commandlineparser/commandline
+1. A GitHub Classroom assignment with feedback option (Providing feedback for an assignment)[https://docs.github.com/en/education/manage-coursework-with-github-classroom/teach-with-github-classroom/create-an-individual-assignment#providing-feedback-for-an-assignment]
+
+2. A GH CLI installed https://cli.github.com/
+
+3. A csv file with columns for:
+   1.  student's answer repo folder
+   2.  a comment text
+
+The tool assumes that when it is ran the user (the teacher) has logged in to GH.
+Use `gh auth login` command or create a token and set it to environment variables.
+
+## External libraries used
+
+CsvHelper https://www.nuget.org/packages/CsvHelper/
+CommandLineParser https://www.nuget.org/packages/CommandLineParser/ 
+
+
+## Links
+
+To login to GH
+- (gh auth login)[https://cli.github.com/manual/gh_auth_login]
+- (Creating a personal access token)[https://docs.github.com/en/github/authenticating-to-github/keeping-your-account-and-data-secure/creating-a-personal-access-token]
+
